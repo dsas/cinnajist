2d851c25857eb0f1bb9c87effe2033b152399039
Dean Sas <dean@deansas.org>
2012-12-08 01:36:14 +0000

    Fetches public gists from the github API
    Doesn't do anything with them yet

--- cinnajist@deansas.org/applet.js
+++ cinnajist@deansas.org/applet.js
@@ -1,8 +1,27 @@
+// Imports:
 const Lang = imports.lang;
 const Applet = imports.ui.applet;
 const PopupMenu = imports.ui.popupMenu;
-const Gettext =  imports.gettext.domain('cinnagist');
-const _ = Gettext.gettext;
+const Json = imports.gi.Json;
+const Soup = imports.gi.Soup;
+const GLib = imports.gi.GLib;
+
+const UUID = 'cinnajist@deansas.org';
+
+
+// Logging - make more convenient
+// Both functions taken from cinnamon-weather
+function log(message)
+{
+	global.log(UUID + "::" + log.caller.name + message);
+}
+function logError(error)
+{
+	global.logError(UUID + "::" + logError.caller.name + ":" + error);
+}
+
+
+const GIST_LIST_URL = 'https://api.github.com/gists';
 
 // All applets are recommended to be called MyApplet..
 function MyApplet(metadata, orientation)
@@ -18,24 +37,60 @@ MyApplet.prototype = {
 		Applet.TextApplet.prototype._init.call(this, orientation);
 		this.set_applet_label("Gists");
 
-		// Need to figure out how to make the menu display
-		this.menuManager = new PopupMenu.PopupMenuManager(this);
-		this.menu = new Applet.AppletPopupMenu(this, orientation);
-		this.menu.addMenuItem(new PopupMenu.PopupMenuItem(_("Loading...")));
-		this.menuManager.addMenu(this.menu);
+		// Setup basic menu
+		this._menuManager = new PopupMenu.PopupMenuManager(this);
+		this._menu = new Applet.AppletPopupMenu(this, orientation);
+		this._menu.addMenuItem(new PopupMenu.PopupMenuItem("Loading..."));
+		this._menuManager.addMenu(this._menu);
+
+		// Setup soup for HTTP requests
+		this._httpSession = new Soup.SessionAsync();
+		Soup.Session.prototype.add_feature.call(
+			this._httpSession, new Soup.ProxyResolverDefault());
+
+		// Kick off an async request
+		this.refresh_gists();
 	},
 
 	on_applet_clicked: function(event)
 	{
-		global.log("Applet clicked");
-		gists = this.get_gists();
+		this.refresh_gists();
+		this._menu.toggle();
 	},
 
-	get_gists: function ()
+	refresh_gists: function ()
 	{
-		return [];
+		this._get_request(GIST_LIST_URL, function(gists)
+			{
+				log("got the gists");
+				gists.foreach_element(function(arr, index, node)
+					{
+						log(node.get_object().get_string_member('description'));
+						log(node.get_object().get_string_member('url'));
+						log(node.get_object().get_object_member('user').get_string_member('login'));
+						// Stuff login and descripton into a menu button and on click open up a web browser
+					});
+			});
+	},
+
+	/**
+	 * Performs a GET request on the specified URL
+	 * url: URL to request
+	 * fn: Function to run if successful, passed response as JSON
+	 */
+	_get_request: function (url, fn)
+	{
+		log(url);
+		let message = Soup.Message.new('GET', url);
+		var closure = Lang.bind(this, function(session, message)
+			{
+				let jp = new Json.Parser();
+				jp.load_from_data(message.response_body.data, -1);
+				fn.call(this, jp.get_root().get_array());
+			});
+		this._httpSession.queue_message(message, closure);
 	}
-}
+};
 
 function main(metadata, orientation)
 {
